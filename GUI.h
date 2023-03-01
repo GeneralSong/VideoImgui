812ba40966475adb20ed6a70d33f9586b3e9107a
songshouliang <songshouliang@vsvec.com>
2023-03-01 22:46:15 +0800

    demo

--- GUI.h
+++ GUI.h
@@ -1,6 +1,51 @@
 #pragma once
+#include "imgui/imgui.h"
+#include <vector>
 
+std::vector<int> DragList;
 void DrawGUI() {
+    ImGui::Begin("window");
+    if (ImGui::IsKeyPressed(ImGuiKey_Q) && ImGui::IsItemHovered()) {
+        ImGui::Text("hello");
+    }
+
+    for (int i = 0; i < 5; i++) {
+        ImGui::Button(std::to_string(i).c_str());
+        if (i + 1 < 5) {
+            ImGui::SameLine();
+        }
+        if (ImGui::BeginDragDropSource()) {
+            ImGui::Text(std::string("Drag : ").append(std::to_string(i)).c_str());
+            ImGui::SetDragDropPayload("DragIndexButton", &i, sizeof(i));
+            ImGui::EndDragDropSource();
+        }
+    }
+
+
+    ImGui::End();
+
+    ImGui::Begin("Drag WIndow");
+
+    ImGui::Text("Drag Target");
+
+    if (ImGui::BeginDragDropTarget()) {
+        if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("DragIndexButton")) {
+
+            DragList.push_back(*(const int*)payload->Data);
+        }
+        ImGui::EndDragDropTarget();
+    }
+
+    for (int i = 0; i < DragList.size(); i++) {
+        if (ImGui::Button(std::to_string(DragList[i]).c_str())) {
+            DragList.erase(DragList.begin() + i);
+        }
+        if (i + 1 < DragList.size()) {
+            ImGui::SameLine();
+        }
+    }
+
+    ImGui::End();
 
 
 
--- main.cpp
+++ main.cpp
@@ -28,6 +28,24 @@ int main(int argc, char* argv[]) {
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.Fonts->AddFontFromFileTTF("arial.ttf",18,nullptr,io.Fonts->GetGlyphRangesChineseFull());
 
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiViewportFlags_NoDecoration;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+    io.ConfigFlags |= ImGuiCol_DockingEmptyBg;
+
+    ImGui::StyleColorsDark();
+    ImGuiStyle& style = ImGui::GetStyle();
+
+    ImVec4* colors = style.Colors;
+    colors[ImGuiCol_FrameBg] = ImVec4(0.0f, 1.0f, 0.0f, 1.0f);
+    colors[ImGuiCol_CheckMark] = ImVec4(0.0f, 0.0f, 0.81f, 1.0f);
+
+    style.WindowRounding = 4;
+    style.FrameRounding = 4;
+    style.GrabRounding = 3;
+    style.ScrollbarSize = 7;
+    style.ScrollbarRounding = 0;
+    
     ImGui::StyleColorsDark();
     ImGui_ImplGlfw_InitForOpenGL(Windows, true);
     ImGui_ImplOpenGL3_Init("#version 330");
@@ -41,6 +59,7 @@ int main(int argc, char* argv[]) {
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
+        ImGui::DockSpaceOverViewport();
 
         DrawGUI();
 
@@ -48,6 +67,15 @@ int main(int argc, char* argv[]) {
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
+
+
 
         glfwSwapBuffers(Windows);
         glfwPollEvents();
